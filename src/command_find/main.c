133867440dcf0da3efb4e4290e4146ba692d5fac
Leo Luttenberger <leo.luttenberger@gmail.com>
2017-02-27 21:05:30 +0100

    changed structur in class

--- src/command_find/main.c
+++ src/command_find/main.c
@@ -10,8 +10,8 @@ void do_dir(const char * dir_name, const char * const * parms);
 int is_dot_or_dot_dot(char const* name);
 int main(int argc, char* argv[])
 {
-    if(argc>2){
-        do_dir(argv[1],argv);
+    if(argc>=2){
+        do_file(argv[1],argv); //do_file statt do_dir
     }
     return 0;
 }
@@ -22,43 +22,19 @@ void do_dir(const char * dir_name, const char * const * parms)
     int paramCase=0;
     DIR* currentDir = opendir(dir_name);
     struct dirent *curr_ent;
-    if(strcmp(parms[2],PARMS1))
-    {
-        paramCase=1;
-    }
     if ( currentDir == NULL )
     {
-        do_file(dir_name,parms);
         return;
     }
-
     while((curr_ent =readdir(currentDir)) != NULL)
     {
-
-        switch(paramCase)
-        {
-        case 1:
-            if(curr_ent->d_type == DT_REG)
-            {
-                return;
-            }
-            else
-            {
-                printf("%s\n", curr_ent->d_name);
-            }
-            break;
-        default:
-            printf("%s\n", curr_ent->d_name);
-
-        }
-
         if(!is_dot_or_dot_dot(curr_ent->d_name)&&curr_ent->d_type == DT_DIR)
         {
             subdir = malloc(strlen(dir_name) + strlen(curr_ent->d_name) + 2);
             strcpy(subdir, dir_name);
-            strcat(subdir, "/");
+            strcat(subdir, "/"); // slash abfrage!
             strcat(subdir, curr_ent->d_name);
-            do_dir(subdir,parms);
+            do_file(subdir,parms); //do_file
             free(subdir);
         }
     }
@@ -68,27 +44,50 @@ int is_dot_or_dot_dot(char const* name)
 {
     return (strcmp(name, ".") == 0 || strcmp(name, "..") == 0 );
 }
-
 void do_file(const char * file_name, const char * const * parms){
 
-    int paramCase=0;
+    int paramCase=0,j=2,i=0;
     struct stat file_stats;
     if(!strcmp(file_name,""))
     {
         return;
     }
-    stat(file_name, &file_stats);
-
-    if(!strcmp(parms[2],PARMS1))
+    //Abfrage der länge des Arrays parms!
+    while(parms[i]!=NULL)
     {
-
-        paramCase=1;
+        i++;
     }
-    switch(paramCase)
+    if(i<j)
     {
-    case 1:
-        printf("\n%s\n", file_name);
-        break;
-
+        printf(" %s ", file_name);
+        return;
     }
+    stat(file_name, &file_stats); //stat lstat?
+
+    while(parms[j]!=NULL)
+    {
+        if(!strcmp(parms[i],PARMS1))
+        {
+
+            paramCase=1;
+        }
+        if(!strcmp(parms[i],PARMS2))
+        {
+            paramCase=2;
+        }
+
+        switch(paramCase)
+        {
+        case 1:
+            printf(" %s ", file_name);
+            break;
+        case 2:
+            printf(" %d ", file_stats.st_uid);
+            break;
+
+        }
+        j++;
+    }
+    do_dir(file_name,parms);
 }
+
